788e0281176083c615545060903884e1f7c5685d
IhorDemchuk <delux11997@gmail.com>
2021-08-23 01:51:57 +0300

    Add all courses page

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 $(document).ready(function () {
+
+   //Burger
+
    $('.burger').click(function () {
       $(this).toggleClass('active');
    })
+
+   //Burger
+
+
+
+   //Choose faculty filter
+
+   $('.faculty_list_item').click(function (e) {
+      e.preventDefault();
+      $(".faculty_list_item").removeClass('active');
+      $(this).addClass('active');
+   })
+
+   //Choose faculty filter
+
 })
+
+
+
+
